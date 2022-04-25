b8bc36c8035a51e85f1d0952de23aab997ea6058
Daniel Peretz <81643329+DanielPeretz30@users.noreply.github.com>
2022-04-25 22:40:05 +0300

    Update dup.py

--- dup.py
+++ dup.py
@@ -1,13 +1,12 @@
-n=257
+n=2570
 arr = []
 result = 0
 exc_res = 0
 for i in range(1,n):
     arr.append(i)
-dup = 16
+dup = 1655
 arr.append(dup)
-for i in range(n-1):
-    exc_res = exc_res^(i+1)
+for i in range(n):
+    exc_res = exc_res^(i)
     result = result ^ arr[i]
-result = result ^ arr[n-1]
 print(result^exc_res)
